edb501013897cfa20433b031a859cfe0a69f355e
NouranElsyed <es-nournelsayed54@alexu.edu.eg>
2024-08-01 20:07:46 +0300

    Q1

---
+++ Assignment/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+
+namespace Assignment
+{
+    internal class Program
+    {
+        //public static ArrayList reverse(ArrayList arrayList) 
+        //{
+        //    for (int i = 0; i < arrayList.Count / 2; i++) 
+        //    {
+        //        int temp = (int)arrayList[i];
+        //        arrayList[i] = arrayList[arrayList.Count - i - 1];
+        //        arrayList[arrayList.Count - i - 1] = temp;
+        //    }
+        //    return arrayList;
+        //}
+        static void Main(string[] args)
+        {
+            #region Part 1
+            /*
+             1.Arraylist
+            hold various data types in one place 
+
+
+
+
+             */
+            //ArrayList arrayList = new ArrayList();
+            // arrayList.Add(1);
+            // arrayList.Add(2);
+            // Console.WriteLine($"Count {arrayList.Count} Capacity {arrayList.Capacity}");
+
+            #endregion
+
+
+            #region Part 2
+
+            #region Q1
+            //ArrayList arraylist = new ArrayList(4) { 1,2,3,4};
+            //foreach (int i in reverse(arraylist)) 
+            //{
+            //    Console.WriteLine(i);
+            //}
+            #endregion
+
+            #region Q2
+
+
+            #endregion
+
+            #region Q3
+
+
+            #endregion
+
+            #endregion
+
+
+
+        }
+    }
+}
